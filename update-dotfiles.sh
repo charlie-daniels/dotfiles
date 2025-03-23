3a40dd2f4a50544e47584ce6c620d007b00a951d
Charlie Daniels <charlied030401@gmail.com>
2025-03-23 22:58:46 +0000

    add update script

---
+++ update-dotfiles.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Array of folders/files to update to Github
+declare -a needUpdate=(".config/waybar" "images/backgrounds" ".config/hypr" ".config/kitty" ".config/theme")
+
+# Local dotfiles folder
+dotfilesPath="$HOME/documents/configs/dotfiles"
+
+# Iterate through each file/folder and replace to the dotfiles folder
+for f in "${needUpdate[@]}"; do
+	rm -rf "${dotfilesPath}/${f}"
+	mkdir -p "$(dirname "${dotfilesPath}/${f}")" && cp -r "$HOME/${f}" "${dotfilesPath}/${f}"
+done
+
+
+# List all installed packages
+pacman -Qe > "${dotfilesPath}/packages.txt"
